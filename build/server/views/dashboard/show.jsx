2a02d7cb02cdfde95240a4f8a729f80e3ffcc45c
marcykay <mahchinkok@gmail.com>
2019-09-11 13:42:47 +0800

    ran build for heroku deployment

---
+++ build/server/views/dashboard/show.jsx
@@ -0,0 +1,18 @@
+var React = require("react");
+
+class Show extends React.Component {
+  render() {
+    return (
+      <html>
+        <head />
+        <body>
+          <div>
+
+          </div>
+        </body>
+      </html>
+    );
+  }
+}
+
+module.exports = Show;
