8b0f2dc11b3a0c22dd6608161b0caf20c34f3be1
marcykay <mahchinkok@gmail.com>
2019-09-11 15:33:31 +0800

    deleted build and updated package.json

--- src/server/middlewares/production.js
+++ src/server/middlewares/production.js
@@ -9,7 +9,6 @@ module.exports = function setup(app) {
   app.use('/', express.static(clientBuildPath));
 
   // all other requests be handled by UI itself
-  app.get('/gogo', (req, res) => res.sendFile(resolve(clientBuildPath, 'index.html'));
+  app.get('/gogo', (req, res) => res.sendFile(resolve(clientBuildPath, 'index.html')));
 
-  });
 };
